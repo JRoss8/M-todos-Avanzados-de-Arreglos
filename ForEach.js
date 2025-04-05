45dffa92a2af7dbba9bde308c63d7e4bab7bacf8
JRoss8 <jorgeros2005@gmail.com>
2025-04-05 12:36:25 -0600

    ForEach

---
+++ ForEach.js
@@ -0,0 +1,12 @@
+function enviarCorreoConfirmacion(asistentes) {
+  asistentes.forEach(asistente => {
+mensaje)
+    console.log(`Enviando correo de confirmación a: ${asistente}`);
+    
+    console.log(`El correo de confirmación ha sido enviado a: ${asistente}`);
+  });
+}
+
+const asistentes = ["Ana", "Luis", "Carlos", "Marta"];
+
+enviarCorreoConfirmacion(asistentes);
